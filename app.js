5ba8070ebde61a0f80a9e3fe7c87b274d3e791f5
brigittecodes <brigittecoaguila@gmail.com>
2025-02-16 20:14:58 -0500

    primer

---
+++ app.js
@@ -0,0 +1,9 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaNombres = [];
+
+function agregarAmigo(){
+    let elementoNombre = document.getElementById('amigo').value;
+    return elementoNombre.value;
+}
+
+console.log(print (elementoNombre))
