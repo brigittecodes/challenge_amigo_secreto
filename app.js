5e401f4d52634db6f87bc106faa695f4252281e3
brigittecodes <brigittecoaguila@gmail.com>
2025-02-18 22:04:39 -0500

    final

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let listaNombres = [];
+let inputAmigo  = document.getElementById('amigo'); //Capturar valor amigo
+let listaAmigos = []; // almacenar nombres de amigos
+let ulistaAmigos = document.getElementById('listaAmigos');
+let ulResultado = document.getElementById('resultado');
+
 
 function agregarAmigo(){
-    let elementoNombre = document.getElementById('amigo').value;
-    return elementoNombre.value;
+     //validar que no esté vacío, sino mostrar un alert para que llene el campo
+    if(amigo == ""){
+        alert("Por favor, inserte un nombre.");
+    }
+    //actualizar el array de amigos
+    listaAmigos.push(inputAmigo.value);
+    ulistaAmigos.innerHTML +=`<li>${inputAmigo.value}</li>`;
+    return ;       
+
 }
 
-console.log(print (elementoNombre))
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+
+function sortearAmigo(){
+    let random = Math.floor(Math.random()*listaAmigos.length);
+    let amigoSecreto = listaAmigos[random];
+    ulResultado.textContent = `El amigo secreto es: ${amigoSecreto}`;
+
+}
